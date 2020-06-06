4d62eaa7f9e226cf2bb7ebe7cc58334b929900e7
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-06 22:49:37 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,10 @@
 # Make your shoe class here!
 class Shoe 
-
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
   def initialize(brand)
     @brand = brand 
   end 
-  
-  def brand 
-    puts "Nike" 
-  end 
-  
-  def initialize(color)
-    @color = color 
-  end 
-  
-  def color 
-    @color 
-  end 
-  
+
 end 
